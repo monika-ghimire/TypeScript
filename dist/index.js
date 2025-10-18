0d4906b9d2d5324c6575507eea2b9ff8abbd2f57
Monika ghimire <monika.ghimirey@boosoft.com.au>
2025-10-18 14:58:17 +0545

    dedug

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log('hello word');
 let age = 20;
+if (age < 20)
+    age += 10;
+console.log(age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 console.log('hello word');
 
 let age : number = 20;
+if(age<20)
+    age += 10;
+console.log(age)
